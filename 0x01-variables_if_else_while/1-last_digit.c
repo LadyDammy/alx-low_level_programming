d638b5e1ab4009d8e8d965ef508db9cded272231
LadyDammy <oreoluwadammie@gmail.com>
2022-09-10 03:58:30 -0700

    print last didit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  * Description: using the main function
  * this program prints "Programming last digit of number
  * Return: 0
- */ 
+*/ 
 int main(void)
 {
 int n;
@@ -20,7 +20,7 @@ n = rand() - RAND_MAX / 2;
 l = n % 10;
 
 if (l > 5)
-{	
+{ 
 	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l == 0)
@@ -28,8 +28,8 @@ else if (l == 0)
 	printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
-{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-}
+{                                                                           
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n,);
+}	
 return (0);
 }
