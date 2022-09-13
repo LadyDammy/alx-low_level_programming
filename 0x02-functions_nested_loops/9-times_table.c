218d21b2011f9a6cc9dfbdddee5c7dd925a58217
LadyDammy <oreoluwadammie@gmail.com>
2022-09-13 13:47:40 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,12 +13,14 @@ for (num = 0; num <= 9; num++)
 {
 	_putchar(',');
 	_putchar(' ');
+
 	prod = num * mult;
+
 if (prod <= 9)
 	_putchar(' ');
 else
 	_putchar((prod / 10) + '0');
-	_putchar((prod % 10) + '0');
+_putchar((prod % 10) + '0');
 }	
 	_putchar('\n');
 }
