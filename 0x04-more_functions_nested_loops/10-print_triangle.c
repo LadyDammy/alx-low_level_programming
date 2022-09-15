ae106034fc703bf7f0a6503390d6e937ee68dffc
LadyDammy <oreoluwadammie@gmail.com>
2022-09-15 13:31:46 -0700

    print a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,10 @@
 void print_triangle(int size)
 {
 int inc1, inc2;
+
 if (size  > 0)
 {
-	for (inc1 = 1; inc1 <= size; inc1++)	
+for (inc1 = 1; inc1 <= size; inc1++)	
 {
 	for ((inc2 = size - inc1); inc2 > 0; inc2--)
 {
@@ -28,7 +29,7 @@ if (incl == size)
 }
 	putchar('\n');
 }
-	}
+}
 }
 	putchar('\n');
 }		
