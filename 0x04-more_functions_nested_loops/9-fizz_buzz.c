0d6027af737000af819637f0809188c7fd091498
LadyDammy <oreoluwadammie@gmail.com>
2022-09-15 10:11:49 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,22 @@
 
 int main(void)
 {
-	int num = 1;
+int num = 1;
 while (num++ < 100)
 {
 	if ((num % 3 == 0) && (num % 5 == 0))
 {
-	printf( "FizzBuzz  ");
+	printf("FizzBuzz ");
 }
 else if ((num % 3) == 0)
 {
-	printf( "Fizz ");
+	printf("Fizz ");
 }
 else if ((num % 5) == 0)
 {	
 if (num != 100)
 {
-	printf("Buzz  ");
+	printf("Buzz ");
 }
 else
 {
@@ -33,7 +33,7 @@ else
 }
 else
 {
-	printf( "%d  ", num);
+	printf( "%d ", num);
 }
 }
 	printf("\n");
