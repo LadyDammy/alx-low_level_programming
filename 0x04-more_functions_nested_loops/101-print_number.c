1b2c1e2a6933d56600d3cb61d1d389a284118e07
LadyDammy <oreoluwadammie@gmail.com>
2022-09-16 05:06:08 -0700

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,7 +9,7 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;	
+	unsigned int num = n;
 if (n < 0)
 {
 	_putchar('-');
@@ -20,5 +20,5 @@ if (num > 9)
 {
 	print_number(num / 10);
 }
-_putchar(num % 10 + '0');
+_putchar((num % 10) + '0');
 }
