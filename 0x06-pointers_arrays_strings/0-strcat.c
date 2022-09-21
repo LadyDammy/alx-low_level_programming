ccf6200ef322fa3e3e58faceb173d3f4a5b90739
LadyDammy <oreoluwadammie@gmail.com>
2022-09-21 16:02:39 -0700

    srtcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,9 +8,9 @@
  * Return: resulting string
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 {
-int index = 0, dest_len = 0;
+	int index = 0; dest_len = 0;
 
 	while (dest[index++])
 	{
@@ -20,5 +20,6 @@ int index = 0, dest_len = 0;
 		dest[dest_len++] = src[index];
 	}
 	}
-	return (dest);
+	_putchar (dest);
 }
+
