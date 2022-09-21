be6d484fbed18d10099b3b2cbe5985dda9b6af6f
LadyDammy <oreoluwadammie@gmail.com>
2022-09-21 07:51:35 -0700

    convert srting to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts string to uppercase
+ * @x: input string
+ * Return: string
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+	if  (x[i] >= 97 && x[i] <= 122)
+
+		x[i] = x[i] -32;
+
+		i++;
+	}
+	return (x);
+}
