ad9738817a6dcfb5133f8c12b0f0b87d37d7b99a
LadyDammy <oreoluwadammie@gmail.com>
2022-09-22 09:23:50 -0700

    compare a string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,40 +4,19 @@
  * _strcmp - function that compares strings
  * @s1: first string
  * @s2: second string
- * Return: integer
+ * Return: integer(0 if equal, +ve if s1 > s2, -ve if s1 < s2)
  */
 int _strcmp(char *s1, char *s2)
-
 {
-	int i = 0, diff;
+	int count = 0, count2 = 0;
 
-	while (1)
+	while (count == 0)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
+		if(*(s1 + count2) == '\0' && *(s2 + count2) == '\0'
+				break;
+				count = *(s1 + coun2) - *(s2 + count2);
+				count2++;
+				}
 
-			break;
-	
-		else (s1[i] == '\0')
-	
-		diff = s2[i];
-
-		break;
-
-		if (s2[i] == '\0')
-	
-		diff = s1[i];
-
-		break;
-	
-		else (s1[i] != s2[i])
-	
-		diff = s1[i] - s2[i];
-
-		break;
-		
-		i++;
-	}	
-}
-{	
-	return (diff);
-}
+				return (count);
+				}				
