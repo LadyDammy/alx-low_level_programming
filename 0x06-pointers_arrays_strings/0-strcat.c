230a4df330c72a119964f0d98da7b40c94320989
LadyDammy <oreoluwadammie@gmail.com>
2022-09-22 05:13:33 -0700

    concatenate string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
-int destLength = 0;
-int counter = 0;
+	int i;
+	int destLength = 0;
+	int counter = 0;
 
 	while (dest[counter] != '\0')
 	{
-	destLength++;
-	counter++;
+		destLength++;
+		counter++;
 	}
 
 	for (i = 0; src[i] != '\0'; i++)
 		dest[destLength + i] = src[i];
-		dest[destLength + i] = '\0';
+	dest[destLength + i] = '\0';
 
 
 	return (dest);
