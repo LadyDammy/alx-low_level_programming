7061d82d0e7fa93851219cb80586efc631fa8e1c
LadyDammy <oreoluwadammie@gmail.com>
2022-09-23 10:55:06 -0700

    leet a fuction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,16 +12,16 @@ char *leet(char *x)
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
-	while (x[a])
-	{
-		b = 0;
+while (x[a])
+{
+	b = 0;
 
-	while (b < l)
-	{
-		if (x[a] == tr[b] || x[a] - 32 == tr[b])
+while (b < l)
+{
 
-		      x[a] = trw[b];
-		b++;
+if (x[a] == tr[b] || x[a] - 32 == tr[b])
+x[a] = trw[b];
+	b++;
 	}
 	a++;
 	}
