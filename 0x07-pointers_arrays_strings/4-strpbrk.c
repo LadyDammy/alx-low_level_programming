0dd1330ffd96d83274f1aee6370595eab139f7ec
LadyDammy <oreoluwadammie@gmail.com>
2022-09-26 06:08:05 -0700

    match bytes between strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - seacrches a string for a set of bytes
+ * @s: string to be checked
+ * @accept: string to be searched
+ *
+ * Return: point to the byte in s that matches that of accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (accept[j])
+			return (s + 1);
+	}
+	return (0);
+}
