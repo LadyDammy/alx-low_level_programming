c987d965ba21114578a17fd4e08e3b27af2ab094
LadyDammy <oreoluwadammie@gmail.com>
2022-09-28 13:25:54 -0700

    Raise the power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * int _pow_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: number to be used
  * @y: exponent to be used
  *
- * return: x ^ y
+ * Return: x ^ y
  */
 
 int _pow_recursion(int x, int y)
