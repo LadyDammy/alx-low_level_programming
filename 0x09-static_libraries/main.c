e917554c4f26852f261b8171f2e0d57ca1065bf8
LadyDammy <oreoluwadammie@gmail.com>
2022-09-30 15:58:10 -0700

    Task 0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,13 @@
 #include "main.h"
 
+/**
+ * main - prints a string
+ *
+ * Return: void
+ */
+
 int main(void)
-{	
-	 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- kevin mitnick");
-	 return (0);
-} 
+{
+	_puts("\"At the end of the day, my goal was to be the best \"\n\t- Dammy");
+return (0);
+}
