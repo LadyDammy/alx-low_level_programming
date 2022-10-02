e74f4d5d780ac8978e2cf9728bbd195f9ae01523
LadyDammy <oreoluwadammie@gmail.com>
2022-10-02 08:36:54 -0700

    WRITE A PROGRAM THAT MULTIPLIES

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,14 @@ int main(int argc, char *argv[])
 {
 	int num_1, num_2, mul;
 
-	if (arg != 3)
+	if (argv != 3)
 		printf("Error\n");
 	else
 	{
 		num_1 = atoi(argv[1]);
-
 		num_2 = atoi(argv[2]);
 		mul = num_1 * num_2;
-		prntf("%d\n", mul);
+		printf("%d\n", mul);
 	}
 	return (0);
 }
