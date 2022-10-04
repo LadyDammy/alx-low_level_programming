93079265648f2b3223886d3a8119dbdbbe6ba499
LadyDammy <oreoluwadammie@gmail.com>
2022-10-04 14:49:48 -0700

    Return pointer

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 	char *cpy;
 	int index, len;
 
-	if (str == NULL);
+	if (str == NULL)
 		return (NULL);
 	for (index = 0; str[index]; index++)
 		len++;
@@ -25,6 +25,7 @@ char *_strdup(char *str)
 	{
 		cpy[index] = str[index];
 	}
+
 	cpy[len] = '\0';
 
 	return (cpy);
