e30cd95f98f4846261bf65995a784492425a25c5
LadyDammy <oreoluwadammie@gmail.com>
2022-10-06 16:16:32 -0700

    Reallocate memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to previously allocated memory
+ * @old_size: size of allocated space for ptr
  * @new_size: size of newly allocated space
  *
  * Return: pointer to newly allocated memory, or NULL if failure
