6a6ab27f2548dc839692627472a71dcd23f7ea02
LadyDammy <oreoluwadammie@gmail.com>
2022-10-12 09:07:40 -0700

    Index an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	
 	if (size > 0 && array && cmp)
 	{
-		for (i = 0, i < size; i++)
+		for (i = 0; i < size; i++;)
 		{
 			r = cmp(array[i]);
 			if (r)
