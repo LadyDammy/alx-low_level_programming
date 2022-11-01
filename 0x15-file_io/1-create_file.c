67ef38f352d39f1b94cc86daeee3c2e6d6f9c52e
LadyDammy <oreoluwadammie@gmail.com>
2022-10-31 21:30:10 -0700

    Create files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	int file, wrote;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		close(file);
-		return(1);
+		return (1);
 	}
 	else
 	{
