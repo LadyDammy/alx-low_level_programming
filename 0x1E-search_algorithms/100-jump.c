0a04127f0840eed76c50ca4dafbcd2b6461c162f
LadyDammy <oreoluwadammie@gmail.com>
2023-04-13 07:47:15 -0700

    Jsa hotfix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 {
 	size_t i, start = 0, end = sqrt(size);
 
-	if (array == NULL || size = 0)
+	if (array == NULL || size == 0)
                 return (-1);	
 
 	while (array[end] < value && start < size){
